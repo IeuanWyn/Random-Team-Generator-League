cc3ec9c00661fcc116b166c30d6dd322736e5b88
Ieuan Rowlands <ieuan.rowlands@gmail.com>
2022-05-20 14:12:33 +0100

    Removing more loggin

--- Program.cs
+++ Program.cs
@@ -5,24 +5,18 @@ using log4net;
 using log4net.Config;
 using System.Reflection;
 
-var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
-XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
-
 MainAsync().GetAwaiter().GetResult();
 
 
 
 static async Task MainAsync()
 {
-    ILog logger = LogManager.GetLogger(typeof(Program));
     var discordToken = Environment.GetEnvironmentVariable("DISCORD_BOT_ID");
     if(discordToken == "SET_TOKEN")
     {
-        logger.Error("Unable to find Environment Variable DISCORD_BOT_ID");
         throw new ArgumentNullException(nameof(discordToken));
     }
 
-    logger.Info("Attempting to connect to Discord Client");
     var discord = new DiscordClient(new DiscordConfiguration()
     {
         Token = discordToken,
@@ -30,7 +24,7 @@ static async Task MainAsync()
         Intents = DiscordIntents.AllUnprivileged,
         MinimumLogLevel = LogLevel.Debug
     });
-    logger.Info("Connected to Discord Client");
+
     var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
     {
         StringPrefixes = new[] { "!" }
@@ -39,6 +33,5 @@ static async Task MainAsync()
     commands.RegisterCommands<TeamGeneratorCommands>();
     
     await discord.ConnectAsync();
-    logger.Info("Completed");
     await Task.Delay(-1);
 }
